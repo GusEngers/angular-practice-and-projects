1c2c4afe77ce13f5d5adfaaafb38649262cc9b54
GusEngers <gustavoengers6441@gmail.com>
2023-01-02 11:27:46 -0300

    ngClass - ngFor - pruebas

--- my-app/src/app/body/body.component.ts
+++ my-app/src/app/body/body.component.ts
@@ -3,9 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-body',
   templateUrl: './body.component.html',
-  styleUrls: ['./body.component.css']
+  styleUrls: ['./body.component.css', './a.component.css']
 })
 export class BodyComponent {
   show:boolean = true;
   count:number = 0;
+  saludos:string[] = ["hola", "buenos días", "chau"];
 }
