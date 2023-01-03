112bf2f886c2cbb606cd3231c192166280a619cd
GusEngers <gustavoengers6441@gmail.com>
2023-01-02 22:59:25 -0300

    añadiendo rutas
    -Configurando las rutas
    -Añadiendo el head con su estilo

--- projects/src/app/app.module.ts
+++ projects/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContadorComponent } from './contador/contador.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContadorComponent
+    ContadorComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
