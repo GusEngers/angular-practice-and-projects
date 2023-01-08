39aedb3016c1d76cd4d8733fe666ef4ef0fc10da
GusEngers <gustavoengers6441@gmail.com>
2023-01-07 22:29:08 -0300

    arreglos, configuracion angular material y nuevos estilos

--- projects/src/app/home/home.component.ts
+++ projects/src/app/home/home.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-
+  projects = [
+    {
+      link: "/contador",
+      name: "Contador"
+    },
+    {
+      link: "/user-list",
+      name: "Lista de Usuarios"
+    }
+  ]
 }
