3e2b9c4a4d59afcba313fdb888db839819ff86b2
GusEngers <gustavoengers6441@gmail.com>
2023-01-20 23:56:25 -0300

    creando primeros inputs y validators

--- projects/src/app/app.module.ts
+++ projects/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -33,6 +33,7 @@ import { ReactiveFormComponent } from './components/reactive-form/reactive-form.
     BrowserAnimationsModule,
     AngularMaterialModule,
     FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- projects/src/app/components/reactive-form/reactive-form.component.ts
+++ projects/src/app/components/reactive-form/reactive-form.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
   templateUrl: './reactive-form.component.html',
-  styleUrls: ['./reactive-form.component.css']
+  styleUrls: ['./reactive-form.component.css'],
 })
-export class ReactiveFormComponent {
+export class ReactiveFormComponent implements OnInit {
+  mainForm: FormGroup = new FormGroup({});
 
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.mainForm = this.formBuilder.group({
+      name: [
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(10),
+        ]),
+      ],
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      password: [
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.minLength(8),
+          Validators.pattern(
+            '^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[*.!@$%^&(){}[]:;<>,.?/~_+-=|]).{8,32}$',
+          ),
+        ]),
+      ],
+      age: ['', Validators.compose([Validators.required, Validators.min(18)])],
+    });
+  }
+
+  get name() {
+    return this.mainForm.get('name');
+  }
+  get email() {
+    return this.mainForm.get('email');
+  }
+  get password() {
+    return this.mainForm.get('password');
+  }
+  get age() {
+    return this.mainForm.get('age');
+  }
 }
