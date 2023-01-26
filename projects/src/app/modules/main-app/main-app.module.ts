fa64fe10e33c8df6e4c42ac4db4330bcbad84189
GusEngers <gustavoengers6441@gmail.com>
2023-01-25 23:20:14 -0300

    modularizando angular material

--- projects/src/app/modules/main-app/main-app.module.ts
+++ projects/src/app/modules/main-app/main-app.module.ts
@@ -3,13 +3,13 @@ import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 // Material Module
-import { AngularMaterialModule } from '../../../material.module';
+import { MaterialModule } from '../material/material.module';
 // Routing Module
 import { AppRoutingModule } from '../../app-routing.module';
 
 @NgModule({
   declarations: [NavBarComponent],
-  imports: [CommonModule, AngularMaterialModule, AppRoutingModule],
+  imports: [CommonModule, MaterialModule, AppRoutingModule],
   exports: [NavBarComponent],
 })
 export class MainAppModule {}
--- projects/src/app/modules/material/material.module.ts
+++ projects/src/app/modules/material/material.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 // Modulos Angular Material
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule],
-  exports: [MatButtonModule, MatToolbarModule],
+  exports: [MatButtonModule, MatToolbarModule, MatIconModule],
 })
 export class MaterialModule {}
